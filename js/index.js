5d06013999417c8dde56eceaaeb9422f776d29af
Roman <roman.gajardo@gmail.com>
2020-10-06 17:58:19 -0300

    arreglos en componentes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 import demo1 from "./modules/demo1.js";
 import demo2 from "./modules/demo2.js";
 import demo3 from "./modules/demo3.js";
+import demo4 from "./modules/demo4.js";
 
 var caja = document.getElementById("caja");
 
 window.saludar = demo1.saludar;
 window.calcular = demo2.calcular;
 window.procesar = demo3.procesar;
+window.cargar = demo4.cargar;
+window.agregar = demo4.agregar.bind(demo4);
+window.buscar = demo4.buscar.bind(demo4);
 
 document.getElementById("demo1").addEventListener("click", () => {
   caja.innerHTML = demo1.data;
@@ -18,4 +22,13 @@ document.getElementById("demo2").addEventListener("click", () => {
 
 document.getElementById("demo3").addEventListener("click", () => {
   caja.innerHTML = demo3.data;
+});
+
+document.getElementById("demo4").addEventListener("click", () => {
+  caja.innerHTML = demo4.data;
+  demo4.cargar();
+});
+
+document.getElementById("demo5").addEventListener("click", () => {
+  caja.innerHTML = demo4.data2;
 });
